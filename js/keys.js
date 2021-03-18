0743ec5d04c802149e732a0c1d776d52a491161c
mstepanek0301 <mstepanek0301@gmail.com>
2021-03-18 10:38:58 +0100

    Added localStorage version check
    If localStorage contains outdated data, it will be cleared

--- js/keys.js
+++ js/keys.js
@@ -1,9 +1,15 @@
 import { year } from '/xmas/js/meta.js'
 
+const version = '5dcca1e2'
 const storage = window.localStorage
 
 export const getKey = number => storage[`key-${year()}-${number}`]
 
 export const addKey = (number, key) => {
   storage[`key-${year()}-${number}`] = key
+}
+
+if (storage['version'] !== version) {
+  storage.clear()
+  storage['version'] = version
 }
